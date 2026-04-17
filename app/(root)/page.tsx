c68676aa954f5d39e4abb1d190177b9fc6b2d1d9
rudraksh <rudraksh711@gmail.com>
2026-04-17 15:19:44 +0530

    added sample book appending and resolve key error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,50 @@
-import React from 'react'
+import React from "react";
 import HeroSection from "@/components/HeroSection";
 import BookCard from "@/components/BookCard";
-import {getAllBooks} from "@/lib/actions/book.actions";
+import { getAllBooks } from "@/lib/actions/book.actions";
 import Search from "@/components/Search";
+import { sampleBooks } from "@/lib/constants";
 
-const Page = async ({ searchParams }: { searchParams: Promise<{ query?: string }> }) => {
-    const { query } = await searchParams;
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const { query } = await searchParams;
 
-    const bookResults = await getAllBooks(query)
-    const books = bookResults.success ? bookResults.data ?? [] : []
+  const bookResults = await getAllBooks(query);
+  // const books = bookResults.success ? bookResults.data ?? [] : []
 
-    return (
-        <main className="wrapper container">
-            <HeroSection />
+  //   append sample books with the user added books
 
-            <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
-                <h2 className="text-3xl font-serif font-bold text-[#212a3b]">Recent Books</h2>
-                <Search />
-            </div>
+  const books = bookResults.success
+    ? [...(bookResults.data ?? []), ...sampleBooks]
+    : sampleBooks;
 
-            <div className="library-books-grid">
-                {books.map((book) => (
-                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
-                ))}
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className="wrapper container">
+      <HeroSection />
 
-export default Page
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+        <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
+          Recent Books
+        </h2>
+        <Search />
+      </div>
+
+      <div className="library-books-grid">
+        {books.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Page;
